1c7f924649d95543515cd768c76b3dade0ea7dc6
Manuel Sande <mnlsande22@gmail.com>
2019-02-15 08:46:17 -0500

    Store user input in array

--- js/triangleTracker.js
+++ js/triangleTracker.js
@@ -1,9 +1,38 @@
-
 function triangleTracker() {
-/*var lengthA = parseInt(prompt('Enter a length'));
+var lengthA = parseInt(document.getElementById('lengthA').value);
+var lengthB = parseInt(document.getElementById('lengthB').value);
+var lengthC = parseInt(document.getElementById('lengthC').value);
+
+lengths = [lengthA, lengthB, lengthC];
+
+for (var index = 0; index<lengths.length; index++) {
+if (lengthA+lengthB<=lengthC || lengthA+lengthC<=lengthB || lengthB+lengthC<=lengthA) {
+alert('The lengths cannot form a triangle');
+break;
+}
+else if (lengthA==lengthB && lengthA==lengthC && lengthB==lengthC) {
+alert('This is an equilateral triangle');
+break;
+}
+else if (lengthA==lengthB || lengthA==lengthC || lengthB==lengthC) {
+alert('This is an isosceles triangle');
+break;
+}
+else {
+alert('This is a scalene triangle');
+break;
+}
+}
+}
+
+
+
+/*
+function triangleTracker() {
+var lengthA = parseInt(prompt('Enter a length'));
 var lengthB = parseInt(prompt('Enter a length'));
 var lengthC = parseInt(prompt('Enter a length'));*/
-  var lengthA = parseInt(document.getElementById('lengthA').value);
+/*  var lengthA = parseInt(document.getElementById('lengthA').value);
   var lengthB = parseInt(document.getElementById('lengthB').value);
   var lengthC = parseInt(document.getElementById('lengthC').value);
 
@@ -21,4 +50,4 @@ var lengthC = parseInt(prompt('Enter a length'));*/
   }
 }
 
-funtionTracker();
+funtionTracker();  */
