f618763dd04b961d166612f0ecbdd4997e4aa8d8
Manuel Sande <mnlsande22@gmail.com>
2019-02-15 07:29:22 -0500

    Fix indentation

--- js/triangleTracker.js
+++ js/triangleTracker.js
@@ -1,26 +1,24 @@
 
-
 function triangleTracker() {
 /*var lengthA = parseInt(prompt('Enter a length'));
 var lengthB = parseInt(prompt('Enter a length'));
 var lengthC = parseInt(prompt('Enter a length'));*/
+  var lengthA = parseInt(document.getElementById('lengthA').value);
+  var lengthB = parseInt(document.getElementById('lengthB').value);
+  var lengthC = parseInt(document.getElementById('lengthC').value);
 
-var lengthA = parseInt(document.getElementById('lengthA').value);
-var lengthB = parseInt(document.getElementById('lengthB').value);
-var lengthC = parseInt(document.getElementById('lengthC').value);
+  if (lengthA+lengthB<=lengthC || lengthA+lengthC<=lengthB || lengthB+lengthC<=lengthA) {
+    alert('The lengths cannot form a triangle');
+  }
+  else if (lengthA==lengthB && lengthA==lengthC && lengthB==lengthC) {
+    alert('This is an equilateral triangle');
+  }
+  else if (lengthA==lengthB || lengthA==lengthC || lengthB==lengthC) {
+    alert('This is an isosceles triangle');
+  }
+  else {
+    alert('This is a scalene triangle');
+  }
+}
 
-if (lengthA+lengthB<=lengthC || lengthA+lengthC<=lengthB || lengthB+lengthC<=lengthA) {
-alert('The lengths cannot form a triangle');
-}
-else if (lengthA==lengthB && lengthA==lengthC && lengthB==lengthC) {
-alert('This is an equilateral triangle');
-}
-else if (lengthA==lengthB || lengthA==lengthC || lengthB==lengthC) {
-alert('This is an isosceles triangle');
-}
-
-else {
-alert('This is a scalene triangle');
-}
-}
 funtionTracker();
