b7d5a376dec06835b2bfac2b4bab09c8da1f2661
Manuel Sande <mnlsande22@gmail.com>
2019-02-16 12:08:41 -0500

    Make fix in accessing array elements

--- js/triangleTracker.js
+++ js/triangleTracker.js
@@ -1,28 +1,29 @@
 
 function triangleTracker() {
+
   var lengthA = parseInt(document.getElementById('lengthA').value);
   var lengthB = parseInt(document.getElementById('lengthB').value);
   var lengthC = parseInt(document.getElementById('lengthC').value);
 
   lengths = [lengthA, lengthB, lengthC];
 
-  for (var index = 0; index<lengths.length; index++) {
-    if (lengthA+lengthB<=lengthC || lengthA+lengthC<=lengthB || lengthB+lengthC
-      <=lengthA) {
-      alert('The lengths cannot form a triangle');
-      break;
+  if (lengths[0]+lengths[1]<=lengths[2] || lengths[0]+lengths[2]<=lengths[1] ||
+      lengths[1]+lengths[2]<=lengths[0]) {
+      alert('Sorry, the lengths cannot form a triangle.');
     }
-    else if (lengthA==lengthB && lengthA==lengthC && lengthB==lengthC) {
-      alert('This is an equilateral triangle');
-      break;
+    else if (lengths[0]==lengths[1] && lengths[0]==lengths[2] &&
+      lengths[1]==lengths[2]) {
+      alert('This is an equilateral triangle.');
     }
-    else if (lengthA==lengthB || lengthA==lengthC || lengthB==lengthC) {
-      alert('This is an isosceles triangle');
-      break;
+    else if (lengths[0]==lengths[1] || lengths[0]==lengths[2] ||
+      lengths[1]==lengths[2]) {
+      alert('This is an isosceles triangle.');
+    }
+    else if (lengths[0]+lengths[1]>lengths[2] || lengths[0]+lengths[2]>lengths[1]
+      || lengths[1]+lengths[2]>lengths[0]) {
+      alert('This is a scalene triangle.');
     }
     else {
-      alert('This is a scalene triangle');
-      break;
+      alert('Sorry, your input cannot be processed.')
     }
-  }
 }
